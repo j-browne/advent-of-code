c11d3c9a073736a38bfa657d32633b99bdfa72f6
Justin Browne <jebdude89@gmail.com>
2021-12-15 01:11:43 -0500

    Add 2021 day_14_02, change polymer

--- 2021/src/polymer.rs
+++ 2021/src/polymer.rs
@@ -2,8 +2,10 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Polymer {
-    /// Element sequence
-    seq: Vec<char>,
+    /// pairs in polymer sequence
+    pairs: HashMap<(char, char), u128>,
+    first: char,
+    last: char,
     /// Insertion rules
     ins_rules: HashMap<(char, char), char>,
 }
@@ -13,7 +15,14 @@ impl Polymer {
     pub fn new(s: &str) -> Self {
         let mut it = s.trim().split("\n\n");
 
-        let seq = it.next().unwrap().chars().collect();
+        let seq = it.next().unwrap().chars().collect::<Vec<_>>();
+        let first = seq[0];
+        let last = seq[seq.len() - 1];
+
+        let mut pairs = HashMap::new();
+        for p in seq.windows(2) {
+            *pairs.entry((p[0], p[1])).or_insert(0) += 1;
+        }
 
         let ins_rules = it
             .next()
@@ -30,17 +39,22 @@ impl Polymer {
             })
             .collect();
 
-        Self { seq, ins_rules }
+        Self {
+            pairs,
+            first,
+            last,
+            ins_rules,
+        }
     }
 
     pub fn insert(&mut self) {
-        let mut old_seq = Vec::new();
-        std::mem::swap(&mut old_seq, &mut self.seq);
-        for x in old_seq.windows(2) {
-            self.seq.push(x[0]);
-            self.seq.push(self.ins_rules[&(x[0], x[1])]);
+        let mut old_pairs = HashMap::new();
+        std::mem::swap(&mut old_pairs, &mut self.pairs);
+        for ((a, c), n) in old_pairs {
+            let b = self.ins_rules[&(a, c)];
+            *self.pairs.entry((a, b)).or_insert(0) += n;
+            *self.pairs.entry((b, c)).or_insert(0) += n;
         }
-        self.seq.push(*old_seq.last().unwrap());
     }
 
     pub fn insert_n(&mut self, n: u32) {
@@ -49,11 +63,22 @@ impl Polymer {
         }
     }
 
+    #[must_use]
     pub fn counts(&self) -> HashMap<char, u128> {
         let mut m = HashMap::new();
-        for c in &self.seq {
-            *m.entry(*c).or_insert(0) += 1;
+        for ((a, b), n) in &self.pairs {
+            *m.entry(*a).or_insert(0) += n;
+            *m.entry(*b).or_insert(0) += n;
         }
+
+        // why +1 and not -1?
+        *m.get_mut(&self.first).unwrap() += 1;
+        *m.get_mut(&self.last).unwrap() += 1;
+
+        for n in m.values_mut() {
+            *n /= 2;
+        }
+
         m
     }
 }
